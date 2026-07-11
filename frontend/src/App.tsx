386175e3891222b18ca20d288d10cb42201a6348
BiniyamTafesse <biniyamtafesse228@gmail.com>
2026-07-11 13:21:30 +0300

    Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,30 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import Login from './pages/Login'
-import './App.css'
 import Signup from './pages/SignUp'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/providers" element={<div>Providers Page</div>} />
       <Route path="/providers/:id" element={<div>Provider Detail Page</div>} />
-      <Route path="/dashboard" element={<div>Dashboard Page</div>} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <div>Dashboard Page</div>
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   )
 }

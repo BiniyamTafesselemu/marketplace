80d7fd17d771bbc863507d2e1401469f846f0336
BiniyamTafesse <biniyamtafesse228@gmail.com>
2026-07-15 11:24:33 +0300

    adding providerservice model and defining its relation with provider profile

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Category = require("./Category");
 const ProviderProfile = require("./ProviderProfile");
+const ProviderService = require("./ProviderService");
 const Booking = require("./Booking");
 const Review = require("./Review");
 const { sequelize } = require("../config/database");
@@ -12,16 +13,18 @@ ProviderProfile.belongsTo(User, { foreignKey: "user_id" });
 Category.hasMany(ProviderProfile, { foreignKey: "category_id" });
 ProviderProfile.belongsTo(Category, { foreignKey: "category_id" });
 
+ProviderProfile.hasMany(ProviderService, { foreignKey: "provider_id" });
+ProviderService.belongsTo(ProviderProfile, { foreignKey: "provider_id" });
+
 User.hasMany(Booking, { foreignKey: "customer_id" });
 Booking.belongsTo(User, { foreignKey: "customer_id" });
 
 Booking.hasOne(Review, { foreignKey: "booking_id" });
 Review.belongsTo(Booking, { foreignKey: "booking_id" });
 
-// Sync all models
 const syncDB = async () => {
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     console.log("All tables synced");
 };
 
-module.exports = { User, Category, ProviderProfile, Booking, Review, syncDB };
+module.exports = { User, Category, ProviderProfile, ProviderService, Booking, Review, syncDB };
